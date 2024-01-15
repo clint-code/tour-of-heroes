25efbee03ca282d63126c7eb8a85d8a3eb1109ed
Clinton Mutinda <cmutinda@mfs.co.ke>
2024-01-15 16:34:41 +0300

    Setting up the selection list of heroes

---
+++ src/app/dist/mock-heroes.js
@@ -0,0 +1,77 @@
+"use strict";
+exports.__esModule = true;
+exports.HEROES = void 0;
+exports.HEROES = [
+    {
+        id: 1,
+        name: 'Batman',
+        alias: 'Bruce Wayne',
+        universe: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Star-Lord',
+        alias: 'Peter Quill',
+        universe: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Spiderman',
+        alias: 'Peter Parker',
+        universe: 'Marvel'
+    },
+    {
+        id: 4,
+        name: 'Wonder Woman',
+        alias: 'Diana Prince',
+        universe: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Captain America',
+        alias: 'Steve Rodgers',
+        universe: 'Marvel'
+    },
+    {
+        id: 6,
+        name: 'Superman',
+        alias: 'Clark Kent',
+        universe: 'DC'
+    },
+    {
+        id: 7,
+        name: 'Black Panther',
+        alias: "T'Challa",
+        universe: 'Marvel'
+    },
+    {
+        id: 8,
+        name: 'Black Widow',
+        alias: 'Natasha Romanoff',
+        universe: 'Marvel'
+    },
+    {
+        id: 9,
+        name: 'Aquaman',
+        alias: 'Arthur Curry',
+        universe: 'DC'
+    },
+    {
+        id: 10,
+        name: 'Green Lantern',
+        alias: 'Hal Jordan',
+        universe: 'DC'
+    },
+    {
+        id: 12,
+        name: 'Iron Man',
+        alias: 'Tony Stark',
+        universe: 'Marvel'
+    },
+    {
+        id: 11,
+        name: 'The Flash',
+        alias: 'Barry Allen',
+        universe: 'DC'
+    }
+];
---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+    id: number;
+    name: string;
+    alias: string;
+    universe: string;
+}
